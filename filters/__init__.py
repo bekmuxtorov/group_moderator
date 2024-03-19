064e14f0566ecc964906c1231ec38680d7f925c1
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-03-19 11:31:57 +0500

    ➕ Added IsPrivate filter

--- filters/__init__.py
+++ filters/__init__.py
@@ -2,7 +2,10 @@ from aiogram import Dispatcher
 
 from loader import dp
 from .group_chat import IsGroup
+from .private_chat import IsPrivate
 
 
 if __name__ == "filters":
     dp.filters_factory.bind(IsGroup)
+    dp.filters_factory.bind(IsPrivate)
+
