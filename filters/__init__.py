91fbf046b3ba4010b19d798678f121cd3927d045
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-03-19 11:28:55 +0500

    ➕ Added IsGroup filter

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,9 +1,8 @@
 from aiogram import Dispatcher
 
 from loader import dp
-# from .is_admin import AdminFilter
+from .group_chat import IsGroup
 
 
 if __name__ == "filters":
-    #dp.filters_factory.bind(is_admin)
-    pass
+    dp.filters_factory.bind(IsGroup)
