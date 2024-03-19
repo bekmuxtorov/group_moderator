bc5a3787e5d143e3f0ac775f94cea68a2b145796
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-03-19 11:24:07 +0500

    🛠️ Setup project

---
+++ data/config.py
@@ -0,0 +1,13 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
+BOT_TOKEN = env.str("BOT_TOKEN")
+ADMINS = env.list("ADMINS")
+IP = env.str("ip")
+
+DB_USER = env.str("DB_USER")
+DB_PASS = env.str("DB_PASS")
+DB_NAME = env.str("DB_NAME")
+DB_HOST = env.str("DB_HOST")
