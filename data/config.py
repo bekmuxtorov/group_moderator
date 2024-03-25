e3ac3c2db3526918196cb3279f6f1e2b76dfda9f
bekmuxtorov <asadbekmuxtorov2@gmail.com>
2024-03-25 16:29:53 +0500

    ➕ Added some constants

--- data/config.py
+++ data/config.py
@@ -7,9 +7,8 @@ BOT_TOKEN = env.str("BOT_TOKEN")
 ADMINS = env.list("ADMINS")
 IP = env.str("ip")
 
-UNTIL_DATE = 1 # 2
-MAX_ATTEMPT_FOR_BLOCK = 4
-COUNT_FOR_READ_ONLY = 2 
+FIRST_RO_TIME = 5
+SECOND_RO_TIME = 3 * 24 * 60
 
 DB_USER = env.str("DB_USER")
 DB_PASS = env.str("DB_PASS")
